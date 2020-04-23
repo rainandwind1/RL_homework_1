24cee095a82b5114827671785cf80905c6f6290b
rainandwind1 <736506946@qq.com>
2020-04-23 21:22:55 +0800

    2020/04/23

--- Inverted_Pole/DQN_py.py
+++ Inverted_Pole/DQN_py.py
@@ -38,7 +38,7 @@ class DQN(nn.Module):
 
     def sample_action(self,state, epsilon):
         input = torch.tensor(state,dtype=torch.float32)
-        input = input.squeeze(0)
+        input = input.unsqueeze(0)
         action_value = self(input)
         coin = np.random.uniform()
         if coin > epsilon:
